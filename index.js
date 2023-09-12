87ccd0c8ddcd97fff65caeba1a8efb339f58e09f
Saurav Chawla <sauravchawla0007@gmail.com>
2023-09-12 16:59:11 +0530

    setting up our assets folder and static files

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const port = 8000;
 
 const expressLayouts = require('express-ejs-layouts');
 
+//app look out for static(assets)
+//app accessing assets folder and its files
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 //use express router
 app.use('/',require('./routes'));
 
