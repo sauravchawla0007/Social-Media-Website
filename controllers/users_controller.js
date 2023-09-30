45a1a35210da0944ad4fe3b0d40248bab4ca0ee9
Saurav Chawla <sauravchawla0007@gmail.com>
2023-09-30 17:16:19 +0530

    uploading files

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -25,15 +25,26 @@ module.exports.update = async function(req, res){
   if(req.user.id == req.params.id){
 
       try{
+        let user = await User.findById(req.params.id);
+        User.uploadedAvatar(req, res, function(err){
+            if (err) {console.log('*****Multer Error: ', err)}
+            
+            user.name = req.body.name;
+            user.email = req.body.email;
 
-          let user = await User.findByIdAndUpdate(req.params.id);
-          
-              
-              user.name = req.body.name;
-              user.email = req.body.email;
+            if (req.file){
 
-              await user.save();
-              return res.redirect('back');
+                if (user.avatar){
+                    fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                }
+
+
+                // this is saving the path of the uploaded file into the avatar field in the user
+                user.avatar = User.avatarPath + '/' + req.file.filename;
+            }
+            user.save();
+            return res.redirect('back');
+        });
        
 
       }catch(err){
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -13,10 +15,28 @@ const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar: {
+        type: String
     }
 },{
     timestamps:true//Timestamps save the current time of the document created and also when it was updated in form of a Date by turning it true
 })
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));//this it the path of uploads/users/avatars
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+  //static function/methods
+  userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+  userSchema.statics.avatarPath = AVATAR_PATH;
+
+
 
 const User = mongoose.model('User',userSchema);
 module.exports = User;
