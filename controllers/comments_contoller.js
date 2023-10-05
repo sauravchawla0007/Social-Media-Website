74115d473d35634c120fc700282cc314fff0d9d5
Saurav Chawla <sauravchawla0007@gmail.com>
2023-10-05 19:39:55 +0530

    setting up kue

--- controllers/comments_contoller.js
+++ controllers/comments_contoller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function (req, res) {
  
@@ -16,7 +18,15 @@ module.exports.create = async function (req, res) {
             post.save();
             comment = await comment.populate('user', 'name email');
             //comment.post.user = await User.findById(comment.post.user);
-            commentsMailer.newComment(comment);
+            //commentsMailer.newComment(comment);
+            let job = queue.create('emails', comment).save(function(err){
+                if (err){
+                    console.log('Error in sending to the queue', err);
+                    return;
+                }
+                console.log('job enqueued', job.id);
+
+            })
             req.flash('success', 'Comment published!');
             res.redirect('/');
         
