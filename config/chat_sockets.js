7d3a178abd3c5dad7d4250c658b3ea630aaeceeb
Saurav Chawla <sauravchawla0007@gmail.com>
2023-10-13 10:08:50 +0530

    creating chat room named social

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -16,6 +16,19 @@ module.exports.chatSockets = function(socketServer){
             console.log('socket disconnected!');
         });
 
+  
+
+      
+    socket.on('join_room', function(data){
+        console.log('joining request rec.', data);
+
+        socket.join(data.chatroom);
+
+        io.in(data.chatroom).emit('user_joined', data);
     });
+
+});
+  
+
 
 }
