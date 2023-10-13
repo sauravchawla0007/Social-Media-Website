dcd78c8dacd88fc251fc6ffac8d4087d0da0e5e6
Saurav Chawla <sauravchawla0007@gmail.com>
2023-10-13 16:04:06 +0530

    sending and receiving messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -2,7 +2,7 @@ class ChatEngine{
     constructor(chatBoxId, userEmail){
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
-      
+
         this.socket = io.connect('http://localhost:5000');
 
         if (this.userEmail){
@@ -30,5 +30,43 @@ class ChatEngine{
 
 
         });
+
+        // CHANGE :: send a message on clicking the send message button
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if (msg != ''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'social'
+                });
+            }
+        });
+
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+
+
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if (data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html': data.message
+            }));
+
+            newMessage.append($('<sub>', {
+                'html': data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
+        })
     }
 }
